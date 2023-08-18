810b265e1ea9f3535a81ac80c82c9af1dbdfbd22
ALEX BUNEA <ALEX BUNEA@DESKTOP-DI4EC9V>
2023-08-18 19:37:31 +0300

    Vite config modification

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,15 @@
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 
+const fullReloadAlways = {
+  handleHotUpdate({ server }) {
+    server.ws.send({ type: "full-reload" })
+    return []
+  },
+}
+
 export default {
+  plugins: [fullReloadAlways],
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
